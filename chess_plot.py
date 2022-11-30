ce94bf556cc9e6543d304d140762a4426e758a0f
OwenMcNaughton <owen.mcnaughton@gmail.com>
2022-11-30 05:28:21 +0000

    B

--- chess_plot.py
+++ chess_plot.py
@@ -1,33 +1,32 @@
 import chess
 import chess.pgn
 import matplotlib.pyplot as plt
-   
-   
+
+
+
 def uci_to_line(uci):
     x1 = uci.from_square % 8
     y1 = uci.from_square // 8
     x2 = uci.to_square % 8
     y2 = uci.to_square // 8
     return [x1, x2], [y1, y2]
-   
-   
+
+
 def draw_line(uci):
     a, b = uci_to_line(uci)
     plt.plot(a, b, linewidth=4, color="r", alpha=0.5)
-   
-   
-   
-def plot_game(game, fna
+
+
+def plot_game(game, fname):
     plt.clf()
-   
+
     board = game.board()
-    for move in game.mainline_move:
+    for move in game.mainline_moves():
         draw_line(move)
-   
+
     plt.savefig(fname)
-   
-   
-   
+
+
 pgn = open("games.pgn")
 iter = 0
 while True:
